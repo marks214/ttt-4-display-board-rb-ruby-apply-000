6979a2ab53f34cdf7c4479eb09243e2b88d04110
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-26 17:09:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board = ["O", "X", " ", " ", "X", " ", "X", "O", " "])
+  puts " #{0} | #{1} | #{2} "
+  puts `-----------`
+  puts " #{3} | #{4} | #{5} "
+  puts `-----------`
+  puts " #{6} | #{7} | #{8} "
+end
